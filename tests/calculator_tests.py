8deb113789cd9ecea6e00c68e06d56964f0a4aab
Yağız Emre ÖZBİLGE <149567624+yagizemreozbilge@users.noreply.github.com>
2024-09-01 11:09:08 +0300

    Add files via upload
    added files

---
+++ tests/calculator_tests.py
@@ -0,0 +1,44 @@
+import unittest
+
+# Hesap makinesi fonksiyonları
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Division by zero is undefined"
+
+# Unit test sınıfı
+class TestCalculator(unittest.TestCase):
+    
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-1, 1), -1)
+        self.assertEqual(multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(-1, 1), -1)
+        self.assertEqual(divide(-1, -1), 1)
+        self.assertEqual(divide(5, 0), "Division by zero is undefined")
+
+if __name__ == "__main__":
+    unittest.main()
